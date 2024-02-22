b2a44e55ba13f5aff06b0985f0ca2bb5e79535c2
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-22 19:14:28 -0300

    style: renamed controller name

--- src/main/java/com/compassuol/sp/challenge/msaddress/domain/web/AddressController.java
+++ src/main/java/com/compassuol/sp/challenge/msaddress/domain/web/AddressController.java
@@ -1,20 +1,21 @@
 package com.compassuol.sp.challenge.msaddress.domain.web;
 
 import com.compassuol.sp.challenge.msaddress.domain.service.AddressService;
-import jakarta.websocket.server.PathParam;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("cep/")
-public class controller {
+public class AddressController {
 
     private AddressService addressService;
 
     @PostMapping("{cep}")
-    public ResponseEntity<Void> saveAddress(@PathParam("cep") String cep){
+    public ResponseEntity<Void> saveAddress(@PathVariable("cep") String cep){
         addressService.save(cep);
         return ResponseEntity.ok().build();
     }
