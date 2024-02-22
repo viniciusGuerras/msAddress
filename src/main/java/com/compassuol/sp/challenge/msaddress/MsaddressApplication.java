d8f5ca34beae076fa5011058ef136f1fdd04fe0d
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-22 18:19:41 -0300

    first iteration

---
+++ src/main/java/com/compassuol/sp/challenge/msaddress/MsaddressApplication.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge.msaddress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaddressApplication.class, args);
+	}
+
+}
