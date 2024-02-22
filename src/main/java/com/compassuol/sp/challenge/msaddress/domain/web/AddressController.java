1149b1ab315f1e94fb0255f65420ab8459cfed0d
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-22 19:40:42 -0300

    feat: created saveAddress endpoint

--- src/main/java/com/compassuol/sp/challenge/msaddress/domain/web/AddressController.java
+++ src/main/java/com/compassuol/sp/challenge/msaddress/domain/web/AddressController.java
@@ -2,6 +2,7 @@ package com.compassuol.sp.challenge.msaddress.domain.web;
 
 import com.compassuol.sp.challenge.msaddress.domain.service.AddressService;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,12 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("cep/")
+@RequestMapping("/cep")
+@RequiredArgsConstructor
 public class AddressController {
 
-    private AddressService addressService;
+    private final AddressService addressService;
 
-    @PostMapping("{cep}")
+    @PostMapping("/{cep}")
     public ResponseEntity<Void> saveAddress(@PathVariable("cep") String cep){
         addressService.save(cep);
         return ResponseEntity.ok().build();
