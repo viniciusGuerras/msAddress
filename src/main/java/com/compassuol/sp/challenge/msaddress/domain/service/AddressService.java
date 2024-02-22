ec7679629e3b1b332eb0bd58491cd1a34930a02b
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-22 19:10:21 -0300

    feat: created service

---
+++ src/main/java/com/compassuol/sp/challenge/msaddress/domain/service/AddressService.java
@@ -0,0 +1,9 @@
+package com.compassuol.sp.challenge.msaddress.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AddressService {
+}
