c789b565e268a743107e1257a7ab52c56ef5331b
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-22 18:30:09 -0300

    feat: basic entity address

---
+++ src/main/java/com/compassuol/sp/challenge/msaddress/domain/Address.java
@@ -0,0 +1,49 @@
+package com.compassuol.sp.challenge.msaddress.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "addresses")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Address {
+    @Id
+    @Column(name = "cep")
+    private String cep;
+
+    @Column(name = "logradouro")
+    private String logradouro;
+
+    @Column(name = "complemento")
+    private String complemento;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Column(name = "localidade")
+    private String localidade;
+
+    @Column(name = "uf")
+    private String uf;
+
+    @Column(name = "ibge")
+    private String ibge;
+
+    @Column(name = "gia")
+    private String gia;
+
+    @Column(name = "ddd")
+    private String ddd;
+
+    @Column(name = "siafi")
+    private String siafi;
+
+}
+
