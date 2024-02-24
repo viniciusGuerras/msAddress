89a39974e65ed9545319552ceffab981e8ccf541
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-23 22:07:00 -0300

    refactor: changed url from /cep to /v1/address

--- src/main/java/com/compassuol/sp/challenge/msaddress/domain/web/AddressController.java
+++ src/main/java/com/compassuol/sp/challenge/msaddress/domain/web/AddressController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cep")
+@RequestMapping("/v1/address")
 @RequiredArgsConstructor
 public class AddressController {
 
