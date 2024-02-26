1187f18bd42faa779aba12e1ea00180e2bca636f
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-26 13:50:39 -0300

    refactor: Changed Path variable to a Body DTO

--- src/main/java/com/compassuol/sp/challenge/msaddress/domain/web/AddressController.java
+++ src/main/java/com/compassuol/sp/challenge/msaddress/domain/web/AddressController.java
@@ -2,23 +2,24 @@ package com.compassuol.sp.challenge.msaddress.domain.web;
 
 import com.compassuol.sp.challenge.msaddress.domain.service.AddressService;
 
+import com.compassuol.sp.challenge.msaddress.domain.web.dto.CepDto;
+import com.compassuol.sp.challenge.msaddress.domain.web.dto.mapper.CepMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/address")
+@Slf4j
 @RequiredArgsConstructor
 public class AddressController {
 
-    private final AddressService addressService;
+    public final AddressService addressService;
 
-    @PostMapping("/{cep}")
-    public ResponseEntity<Void> saveAddress(@PathVariable("cep") String cep){
-        addressService.save(cep);
+    @PostMapping()
+    public ResponseEntity<Void> saveAddress(@RequestBody CepDto cep){
+        addressService.save(CepMapper.toCep(cep));
         return ResponseEntity.ok().build();
     }
 }
