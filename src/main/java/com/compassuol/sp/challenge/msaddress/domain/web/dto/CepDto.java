142008cd947bf541cca35c6b3265964cc38cdaff
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-27 10:10:52 -0300

    feat: created a Cep DTO and mapper

---
+++ src/main/java/com/compassuol/sp/challenge/msaddress/domain/web/dto/CepDto.java
@@ -0,0 +1,12 @@
+package com.compassuol.sp.challenge.msaddress.domain.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CepDto {
+    private String cep;
+}
