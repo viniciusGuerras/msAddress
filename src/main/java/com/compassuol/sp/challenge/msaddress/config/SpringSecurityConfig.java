8c59aa73a4a233b699edee861c925aacc68870ba
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-28 13:40:31 -0300

    style: removed unused import

--- src/main/java/com/compassuol/sp/challenge/msaddress/config/SpringSecurityConfig.java
+++ src/main/java/com/compassuol/sp/challenge/msaddress/config/SpringSecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -49,7 +48,6 @@ public class SpringSecurityConfig {
                 ).exceptionHandling(ex -> ex
                         .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
                 ).build();
-
     }
 
     @Bean
