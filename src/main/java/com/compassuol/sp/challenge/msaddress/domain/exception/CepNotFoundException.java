d924b4e36b8ec8d8b18d6896d18b3fbd343397aa
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-28 22:40:52 -0300

    feat: created new exception

---
+++ src/main/java/com/compassuol/sp/challenge/msaddress/domain/exception/CepNotFoundException.java
@@ -0,0 +1,7 @@
+package com.compassuol.sp.challenge.msaddress.domain.exception;
+
+public class CepNotFoundException extends RuntimeException {
+    public CepNotFoundException(String message) {
+        super(message);
+    }
+}
